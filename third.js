f25e78f45594c1a9ac05591fcea5dc8a74ff66c8
yashi2403 <yashichaudhary2403@gmail.com>
2022-11-12 00:54:40 +0530

    final

---
+++ third.js
@@ -0,0 +1,12 @@
+// const newBackgroundColor = document.querySelector('.box-3');
+// function changeBgColor(){
+// newBackgroundColor.style.backgroundColor ='blue';
+// };
+// newBackgroundColor.addEventListener("mouseover" , changeBgColor);
+ 
+// const newBackgroundColor= document.querySelector('.skillbox');
+// function changeBgColor(){
+//     newBackgroundColor.getElementsByClassName.backgroundColor ='#d6c8ff';
+
+// };
+// newBackgroundColor.addEventListener("mouseover",changeBgColor);
